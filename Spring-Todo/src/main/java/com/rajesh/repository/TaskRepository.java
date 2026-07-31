5e3d4e0135eda20d8b2ef80fe9e46740f204b2ef
Ansh_A <rajesh.in1209@gmail.com>
2026-07-31 23:06:29 +0530

    Update TaskRepository.java

--- Spring-Todo/src/main/java/com/rajesh/repository/TaskRepository.java
+++ Spring-Todo/src/main/java/com/rajesh/repository/TaskRepository.java
@@ -1,7 +1,5 @@
 package com.rajesh.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.rajesh.model.Task;
-
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
