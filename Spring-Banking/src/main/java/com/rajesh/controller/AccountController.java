1276584e19e502dd963d0cfca76d997987498dbc
Ansh_A <rajesh.in1209@gmail.com>
2026-07-31 23:08:53 +0530

    Update AccountController.java

--- Spring-Banking/src/main/java/com/rajesh/controller/AccountController.java
+++ Spring-Banking/src/main/java/com/rajesh/controller/AccountController.java
@@ -1,42 +1,27 @@
 package com.rajesh.controller;
-
 import com.rajesh.entity.Account;
 import com.rajesh.service.AccountService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
-
-
 @RestController
 @RequestMapping("/api/accounts")
 @CrossOrigin(origins = "*")
 public class AccountController {
-
     @Autowired
     private AccountService service;
-
-    // ✅ Create Account
     @PostMapping
     public Account createAccount(@RequestBody Account account) {
         return service.createAccount(account);
     }
-
-    // ✅ Get All Accounts
     @GetMapping
     public List<Account> getAllAccounts() {
         return service.getAllAccounts();
     }
-
-    // ✅ Get Account by ID
     @GetMapping("/{id}")
     public Account getAccount(@PathVariable Long id) {
         return service.getAccount(id);
     }
-
-    // ✅ Update Account
     @PutMapping("/{id}")
     public Account updateAccount(@PathVariable Long id,
                                  @RequestBody Account account) {
